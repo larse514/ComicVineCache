0e07c70bbd49ede86b19b1628bc88bbb05d01d7c
test <test>
2016-07-04 22:47:51 -0500

    updating port

--- redisTest.js
+++ redisTest.js
@@ -1,5 +1,5 @@
 var redis = require('redis');
-var client = redis.createClient(port, host);
+var client = redis.createClient(6379, 'comicvinecache-001.br0imq.0001.usw2.cache.amazonaws.com');
 client.on('connect', function() {
     console.log('connected');
 });
