13337b0ff97faf249568336937413de1b1d95892
test <test>
2016-07-05 18:51:35 -0500

    committing update

--- redisTest.js
+++ redisTest.js
@@ -3,3 +3,13 @@ var client = redis.createClient(6379, 'comicvinecache-001.br0imq.0001.usw2.cache
 client.on('connect', function() {
     console.log('connected');
 });
+
+client.set('test', 'Hello World', function(err, reply) {
+	if(err) console.log(err)
+	console.log(reply);
+});
+
+client.get('test', function(err, reply) {
+	if(err) console.log(err)
+	console.log(reply);
+});
