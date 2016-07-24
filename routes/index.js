2b957d5f379821762efc7704b75459d0c779a950
larse514 <andrew.larse514@gmail.com>
2016-07-24 00:18:24 -0500

    adding json check

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res) {
 //method to getCache value
 router.get('/getCache', function(req, res) {
 	var key = req.query.key
-	if(key){
+	if(typeof key === "string"){
 		cache.prototype.getCache(key, function(value){
 			if(value){
 				//need to run eval to make sure it's an object
